fe2afed2ed9faaebdbe78aba553ae7f9f14a315c
qiujiandong <qiujiandong@nucleisys.com>
2025-09-28 12:05:13 +0800

    update build config
    - Add `-DUSE_PERF_EVENT`
    - build with `_zvf` and `_zvfh` extension
    - restric the max mem to 1M
    
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- bench/config.h
+++ bench/config.h
@@ -1,5 +1,5 @@
 /* the maximum number of bytes to allocate, minimum of 4096 */
-#define MAX_MEM (1024*1024*32)
+#define MAX_MEM (1024*1024*1)
 /* the byte count for the next run */
 #define NEXT(c) (c + c/7 + 3)
 
--- config.mk
+++ config.mk
@@ -1,8 +1,13 @@
 WARN=-Wall -Wextra -Wno-unused-function -Wno-unused-parameter
 
-# native build, adjust -march= to match your platform
-CC=cc
-CFLAGS=-march=rv64gcv -O3 ${WARN} -DUSE_PERF_EVENT
+# append -DUSE_PERF_EVENT to CFLAGS, if the cycle csr isn't exposed
+# try -DUSE_PERF_EVENT_SLOW if the abvoe doesn't work
+
+# freestanding using any recent clang build
+CC=clang
+CFLAGS=--target=riscv64 -march=rv64gcv_zfh_zvfh_zba_zbb_zbs -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding -DUSE_PERF_EVENT
+#CFLAGS=--target=riscv32 -march=rv32gc_zve32f_zba_zbb_zbs -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding
+
 
 # full cross compilation toolchain
 #CC=riscv64-linux-gnu-gcc
