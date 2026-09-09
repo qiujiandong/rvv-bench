6c0738d4e603613bc4ab00e24a82a8531fc00e7e
Jiandong Qiu <qiujiandong@nucleisys.com>
2026-09-09 15:41:28 +0800

    preload VME sgemm operands into tiles
    Interleave tile-to-vector moves with FMM operations.
    
    Signed-off-by: Jiandong Qiu <qiujiandong@nucleisys.com>

--- nuclei_baremetal/common/sgemm_vme.c
+++ nuclei_baremetal/common/sgemm_vme.c
@@ -40,6 +40,7 @@ void _sgemm_vme(float *restrict c, float const *restrict a,
 
   for (size_t row = 0; row < M; ++row) {
     zvt_vtle32(ZVT_TSS_COL_OF(ZVT_MT0, row), a + row * N);
+    zvt_vtle32(ZVT_TSS_ROW_OF(ZVT_MT4, row), b + row * N);
     zvt_vtle32(ZVT_TSS_ROW_OF(ZVT_MT8, row), c + row * N);
   }
 
@@ -48,22 +49,26 @@ void _sgemm_vme(float *restrict c, float const *restrict a,
   size_t k = 0;
   for (; k + 4 <= K; k += 4) {
     __asm__ volatile(".insn r 0x57, 6, 0x21, v8, %[a_col0], x31\n"
+                     ".insn r 0x57, 6, 0x21, v16, %[b_row0], x31\n"
+                     ZVT_C_ASM_WORD(fmm0)
                      ".insn r 0x57, 6, 0x21, v9, %[a_col1], x31\n"
+                     ".insn r 0x57, 6, 0x21, v17, %[b_row1], x31\n"
+                     ZVT_C_ASM_WORD(fmm1)
                      ".insn r 0x57, 6, 0x21, v10, %[a_col2], x31\n"
+                     ".insn r 0x57, 6, 0x21, v18, %[b_row2], x31\n"
+                     ZVT_C_ASM_WORD(fmm2)
                      ".insn r 0x57, 6, 0x21, v11, %[a_col3], x31\n"
-                     "vle32.v v16, (%[b0])\n"
-                     "vle32.v v17, (%[b1])\n"
-                     "vle32.v v18, (%[b2])\n"
-                     "vle32.v v19, (%[b3])\n" ZVT_C_ASM_WORD(fmm0)
-                         ZVT_C_ASM_WORD(fmm1) ZVT_C_ASM_WORD(fmm2)
-                             ZVT_C_ASM_WORD(fmm3)
+                     ".insn r 0x57, 6, 0x21, v19, %[b_row3], x31\n"
+                     ZVT_C_ASM_WORD(fmm3)
                      :
                      : [a_col0] "r"(ZVT_TSS_ROW_OF(ZVT_MT0, k)),
                        [a_col1] "r"(ZVT_TSS_ROW_OF(ZVT_MT0, k + 1)),
                        [a_col2] "r"(ZVT_TSS_ROW_OF(ZVT_MT0, k + 2)),
                        [a_col3] "r"(ZVT_TSS_ROW_OF(ZVT_MT0, k + 3)),
-                       [b0] "r"(b + k * N), [b1] "r"(b + (k + 1) * N),
-                       [b2] "r"(b + (k + 2) * N), [b3] "r"(b + (k + 3) * N),
+                       [b_row0] "r"(ZVT_TSS_ROW_OF(ZVT_MT4, k)),
+                       [b_row1] "r"(ZVT_TSS_ROW_OF(ZVT_MT4, k + 1)),
+                       [b_row2] "r"(ZVT_TSS_ROW_OF(ZVT_MT4, k + 2)),
+                       [b_row3] "r"(ZVT_TSS_ROW_OF(ZVT_MT4, k + 3)),
                        ZVT_C_ASM_IMM(fmm0, ZVT_ENC_VTFMM_TVV(ZVT_MT8, 8, 16)),
                        ZVT_C_ASM_IMM(fmm1, ZVT_ENC_VTFMM_TVV(ZVT_MT8, 9, 17)),
                        ZVT_C_ASM_IMM(fmm2, ZVT_ENC_VTFMM_TVV(ZVT_MT8, 10, 18)),
@@ -74,9 +79,11 @@ void _sgemm_vme(float *restrict c, float const *restrict a,
   for (; k < K; ++k) {
     __asm__ volatile(
         ".insn r 0x57, 6, 0x21, v8, %[a_col], x31\n"
-        "vle32.v v16, (%[b])\n" ZVT_C_ASM_WORD(fmm)
+        ".insn r 0x57, 6, 0x21, v16, %[b_row], x31\n"
+        ZVT_C_ASM_WORD(fmm)
         :
-        : [a_col] "r"(ZVT_TSS_ROW_OF(ZVT_MT0, k)), [b] "r"(b + k * N),
+        : [a_col] "r"(ZVT_TSS_ROW_OF(ZVT_MT0, k)),
+          [b_row] "r"(ZVT_TSS_ROW_OF(ZVT_MT4, k)),
           ZVT_C_ASM_IMM(fmm, ZVT_ENC_VTFMM_TVV(ZVT_MT8, 8, 16))
         : "memory");
   }
